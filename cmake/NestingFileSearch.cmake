e77a1101485be23a88be6900e505e9f2df74609a
Exerros <Mujuryoku@gmail.com>
2022-07-03 19:26:56 +0300

    Update nesting file search

--- cmake/NestingFileSearch.cmake
+++ cmake/NestingFileSearch.cmake
@@ -3,8 +3,8 @@
 
 macro (NestingFileSearch OUTPUT)
     set (options RECURSIVE APPEND)
-    set (oneValueArgs EXTENSION DIRECTORY)
-    set (multiValueArgs "")
+    set (oneValueArgs "")
+    set (multiValueArgs EXTENSIONS DIRECTORIES EXCLUDE_PATTERNS)
     cmake_parse_arguments (NFS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     # Returns an error if the passed values are incorrect
@@ -23,22 +23,36 @@ macro (NestingFileSearch OUTPUT)
         set (GLOB_TYPE GLOB)
     endif ()
 
-    string (REGEX REPLACE "(.*/).*" "\\1" NFS_EXTERNAL_PATH "${NFS_DIRECTORY}")
-    file (${GLOB_TYPE} NFS_LIST_DIRECTORIES LIST_DIRECTORIES TRUE "${NFS_DIRECTORY}/*")
-    list (APPEND NFS_LIST_DIRECTORIES ${NFS_DIRECTORY})
+    foreach (NFS_BASE_DIR ${NFS_DIRECTORIES})
+        string (REGEX REPLACE "(.*/).*" "\\1" NFS_EXTERNAL_PATH "${NFS_BASE_DIR}")
+        file (${GLOB_TYPE} NFS_LIST_DIRECTORIES LIST_DIRECTORIES TRUE "${NFS_BASE_DIR}/*")
+        list (APPEND NFS_LIST_DIRECTORIES ${NFS_BASE_DIR})
 
-    foreach (NFS_DIRECTORY ${NFS_LIST_DIRECTORIES})
-        if (IS_DIRECTORY ${NFS_DIRECTORY})
-            string (REGEX REPLACE "${NFS_EXTERNAL_PATH}(.*)" "\\1" NFS_GROUP_NAME "${NFS_DIRECTORY}")
+        foreach (NFS_DIR ${NFS_LIST_DIRECTORIES})
+            if (IS_DIRECTORY ${NFS_DIR})
+                string (REGEX REPLACE "${NFS_EXTERNAL_PATH}(.*)" "\\1" NFS_GROUP_NAME "${NFS_DIR}")
 
-            file (GLOB NFS_FILES_IN_DIRECTORY LIST_DIRECTORIES FALSE "${NFS_DIRECTORY}/*${NFS_EXTENSION}")
+                foreach (NFS_EXTENSION ${NFS_EXTENSIONS})
+                    file (GLOB NFS_FILES_IN_DIRECTORY LIST_DIRECTORIES FALSE "${NFS_DIR}/*${NFS_EXTENSION}")
+                endforeach ()
 
-            if (MSVC)
-                source_group ("${NFS_GROUP_NAME}" FILES ${NFS_FILES_IN_DIRECTORY})
+                if (MSVC)
+                    source_group ("${NFS_GROUP_NAME}" FILES ${NFS_FILES_IN_DIRECTORY})
+                endif ()
+
+                list (APPEND ${OUTPUT} ${NFS_FILES_IN_DIRECTORY})
             endif ()
+        endforeach ()
+    endforeach ()
 
-            list (APPEND ${OUTPUT} ${NFS_FILES_IN_DIRECTORY})
-        endif ()
+    foreach (PATTERN ${NFS_EXCLUDE_PATTERNS})
+        foreach (VALUE ${${OUTPUT}})
+            string(REGEX MATCH ${PATTERN} IS_MATCHED ${VALUE})
+
+            if(NOT ${IS_MATCHED} STREQUAL "")
+                list(REMOVE_ITEM ${OUTPUT} ${VALUE})
+            endif()
+        endforeach ()
     endforeach ()
 
 endmacro ()
